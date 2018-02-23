45c0d0b06cdb54b32878f460ce38fafa2dd0cd5f
WindWalk12 <nhygta@hotmail.com>
2018-02-23 13:34:09 +0100

    Add files via upload

---
+++ Game-shop/DataAccessLayer/GameDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Game_shop.Models.DataModel;
+
+namespace Game_shop.DataAccessLayer
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext() : base("name=GameDb")
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Game-shop/Models/DataModel/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game_shop.Models.DataModel
+{
+    public class 
+        Game
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
